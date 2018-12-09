cdd1bc44c5d3c36de7ae9fe79e6cfbcb0adeeef9
boot1110001 <boot111.0001@gmail.com>
2018-12-09 22:11:28 +0100

    reestructurado y añadidas variables del entorno

--- tests/basic-functionality.py
+++ tests/basic-functionality.py
@@ -1,17 +1,28 @@
 # VERSION: 20181208a
-# By boot1110001. Based on the script of Josh Schreuder (2011)
+# By boot1110001.
 
-import os #for home dir syntax
+### IMPORTS ############################################################
+import os #for a lot of things
+from os.path import expanduser #to get the HOME user dir in linux
 import re #for regex
 import urllib #to download websites
 import urllib2 #to download websites
 import datetime #to get the time
 import subprocess #to execute BASH commands
 
-os.path.expanduser('~') #to get the home user
 
+### NON EDITABLE VARIABLES #############################################
+HOME = expanduser("~") #to get the HOME user dir in linux
 # time wehe this script is executed in format YYYY-mm-dd-HH-MM-SS
-now=datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") 
+NOW = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+
+### EDITABLE VARIABLES #################################################
+# Write the path to the directory where the APOD images will be saved.
+APOD1_DIR = HOME+"/00617/apod-dybg-py/tests/apod1"
+# Write the path to the directory where the AAPOD2 images will be saved.
+APOD2_DIR = HOME+"/00617/apod-dybg-py/tests/apod2"
+
 
 ### CLASSES ############################################################
 class BgImg:
@@ -46,16 +57,6 @@ def get_page2():
 	matcher=patron.findall(web_content)
 	return 'http://www.aapodx2.com'+matcher[1]
 	
-def download_bg(bg):
-	print("Downloading the image '"+bg.url+"' in '"+bg.loc+"'...")
-	urllib.urlretrieve(bg.url,bg.loc)
-		
-def set_as_bg(bg):
-	bashCommand = 'gsettings set org.gnome.desktop.background picture-uri file://'+bg.loc
-	print('CMD: '+bashCommand)
-	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-	output, error = process.communicate()
-
 def check_url(url):
 	try:
 		resp = urllib2.urlopen(url)
@@ -67,16 +68,39 @@ def check_url(url):
 	else:
 		print "The URL '"+url+"' gave the code:", resp.code, resp.msg
 		return True
+	
+def download_bg(bg):
+	print("Downloading the image '"+bg.url+"' in '"+bg.loc+"'...")
+	urllib.urlretrieve(bg.url,bg.loc)
+		
+def set_as_bg(bg):
+	# The program should detect the desktop environment and select the correct command.
+	bashCommand = 'gsettings set org.gnome.desktop.background picture-uri file://'+bg.loc
+	print('CMD: '+bashCommand)
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
 
-### EXEC ###############################################################
+def clean_old_bgs():
+	print("Cleaning the old background images...")
+	
+def clean_tmp_files():
+	print("Cleaning temporary files...")
+	# Temporary files are not yet saved.
+	
+def clean_all():
+	clean_tmp_files()
+	clean_old_bgs()
+	
+
+### MAIN ###############################################################
 url1=get_page1()
 url2=get_page2()
 
 if (check_url(url1) and check_url(url2)):
 	print("The two URLs are correct and work well.")
-	b1 = BgImg('apod1',url1,'/home/cosmo/00617/apod-dybg-py/tests/apod1',now)
+	b1 = BgImg('apod1',url1,APOD1_DIR,NOW)
 	b1.to_string()
-	b2 = BgImg('apod2',url2,'/home/cosmo/00617/apod-dybg-py/tests/apod2',now)
+	b2 = BgImg('apod2',url2,APOD2_DIR,NOW)
 	b2.to_string()
 
 	download_bg(b1)
