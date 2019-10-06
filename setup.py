4d98b5fc7721053e83c393ffe3db0dbd9b522075
boot1110001 <ppuubblliicc@protonmail.com>
2019-10-06 21:03:20 +0200

    update setup.py to python3

--- setup.py
+++ setup.py
@@ -10,149 +10,149 @@ import stat #for change permissions
 # To get the HOME user dir in linux.
 HOME = os.path.expanduser("~")
 # The version of the program.
-VERSION=''
+VERSION = ""
 with open('version.txt', 'r') as version_file:
     VERSION=version_file.read().replace('\n', '')
 
 ### EXEC #######################################################################
-print "# APOD Dynamic Background ["+VERSION+"] by boot1110001"
-print "# Function: Install or update 'apod-dybg-py'"
-print ""
+print("# APOD Dynamic Background ["+VERSION+"] by boot1110001")
+print("# Function: Install or update 'apod-dybg-py'")
+print("")
 
 # PREVIOUS CHECKS
-print "# Checking if there is already a version of this program installed"
+print("# Checking if there is already a version of this program installed")
 folder=HOME+'/.apod-dybg-py'
 if os.path.exists(folder):
-    print "# Yes, there is another version installed"
+    print("# Yes, there is another version installed")
     try:
         with open(folder+'/version.txt', 'r') as version_file:
             installed_version=version_file.read().replace('\n', '')
             if (VERSION != installed_version):
-                print "# The version is different, deleting and starting again"
+                print("# The version is different, deleting and starting again")
                 shutil.rmtree(folder)
             else:
-                print "# It is the same version, only the updated files are replaced"
+                print("# It is the same version, only the updated files are replaced")
         pass
     except IOError:
-        print "# The version file was not found, deleting and starting again"
+        print("# The version file was not found, deleting and starting again")
         shutil.rmtree(folder)
         pass
 else:
-    print "# No, there is no other version installed"
-print ""
+    print("# No, there is no other version installed")
+print("")
 
 # FOLDER STRUCTURE
-print "### Creating the folder structure"
-print "# Creating the folders for the program (if it does not already exist)"
+print("### Creating the folder structure")
+print("# Creating the folders for the program (if it does not already exist)")
 folder=HOME+'/.apod-dybg-py'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/core'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/bg-default'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/apod-image'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/aapod2-image'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/icons'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/icons/8x8'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/icons/16x16'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/icons/22x22'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/icons/24x24'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/icons/32x32'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/icons/48x48'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/icons/256x256'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 folder=HOME+'/.apod-dybg-py/media/icons/512x512'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 
-print "# Creating the folder for the .desktop file (if it does not already exist)"
+print("# Creating the folder for the .desktop file (if it does not already exist)")
 folder=HOME+'/.local/share/applications'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 
-print "# Creating the folder for the autostar .desktop file (if it does not already exist)"
+print("# Creating the folder for the autostar .desktop file (if it does not already exist)")
 folder=HOME+'/.config/autostart'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
+print(" + "+folder)
 
-print "# Creating the folder for the app icon (if it does not already exist)"
+print("# Creating the folder for the app icon (if it does not already exist)")
 folder=HOME+'/.icons'
 if not os.path.exists(folder): os.makedirs(folder)
-print " + "+folder
-print ""
+print(" + "+folder)
+print("")
 
 # COPYING FILES
-print "### Copying files"
-print "# Copying the program version file"
+print("### Copying files")
+print("# Copying the program version file")
 orig_file='version.txt'
 dest_file=HOME+'/.apod-dybg-py/version.txt'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 
-print "# Copying the main program"
+print("# Copying the main program")
 orig_file='apod-dybg-py.py'
 dest_file=HOME+'/.apod-dybg-py/apod-dybg-py.py'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
-print "# Changing program permissions"
+print(" cp "+dest_file)
+print("# Changing program permissions")
 st = os.stat(dest_file)
 # Consider assigning only the permissions to the owner user.
 # os.chmod(dest_file, st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
 os.chmod(dest_file, st.st_mode | 0111) #the same as avove.
 
-print "# Copiyin the core modules"
+print("# Copiyin the core modules")
 orig_file='core/__init__.py'
 dest_file=HOME+'/.apod-dybg-py/core/__init__.py'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 orig_file='core/clases.py'
 dest_file=HOME+'/.apod-dybg-py/core/clases.py'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 orig_file='core/extra.py'
 dest_file=HOME+'/.apod-dybg-py/core/extra.py'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 orig_file='core/utils.py'
 dest_file=HOME+'/.apod-dybg-py/core/utils.py'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 
-print "# Copiyin the uninstaller"
+print("# Copiyin the uninstaller")
 orig_file='utils/uninstall.py'
 dest_file=HOME+'/.apod-dybg-py/uninstall.py'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
-print "# Changing uninstaller permissions"
+print(" cp "+dest_file)
+print("# Changing uninstaller permissions")
 st = os.stat(dest_file)
 os.chmod(dest_file, st.st_mode | 0111) #consider assigning only the permissions to the owner user.
 
-print "# Copying all the default backgrounds"
+print("# Copying all the default backgrounds")
 orig_folder='media/bg-default'
 dest_folder=HOME+'/.apod-dybg-py/media/bg-default'
 src_files = os.listdir(orig_folder)
@@ -161,76 +161,76 @@ for file_name in src_files:
     if (os.path.isfile(full_file_name)):
         shutil.copy(full_file_name, dest_folder)
 
-print "# Copying all the icons"
+print("# Copying all the icons")
 orig_file='media/icons/8x8/apod-dybg-py.png'
 dest_file=HOME+'/.apod-dybg-py/media/icons/8x8/apod-dybg-py.png'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 orig_file='media/icons/16x16/apod-dybg-py.png'
 dest_file=HOME+'/.apod-dybg-py/media/icons/16x16/apod-dybg-py.png'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 orig_file='media/icons/22x22/apod-dybg-py.png'
 dest_file=HOME+'/.apod-dybg-py/media/icons/22x22/apod-dybg-py.png'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 orig_file='media/icons/24x24/apod-dybg-py.png'
 dest_file=HOME+'/.apod-dybg-py/media/icons/24x24/apod-dybg-py.png'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 orig_file='media/icons/32x32/apod-dybg-py.png'
 dest_file=HOME+'/.apod-dybg-py/media/icons/32x32/apod-dybg-py.png'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 orig_file='media/icons/48x48/apod-dybg-py.png'
 dest_file=HOME+'/.apod-dybg-py/media/icons/48x48/apod-dybg-py.png'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 orig_file='media/icons/256x256/apod-dybg-py.png'
 dest_file=HOME+'/.apod-dybg-py/media/icons/256x256/apod-dybg-py.png'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 orig_file='media/icons/512x512/apod-dybg-py.png'
 dest_file=HOME+'/.apod-dybg-py/media/icons/512x512/apod-dybg-py.png'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
+print(" cp "+dest_file)
 
-print "# Copying the selected icon to the corresponding folder"
+print("# Copying the selected icon to the corresponding folder")
 orig_file='media/icons/256x256/apod-dybg-py.png'
 dest_file=HOME+'/.icons/apod-dybg-py.png'
 shutil.copyfile(orig_file,dest_file)
-print " cp "+dest_file
-print ""
+print(" cp "+dest_file)
+print("")
 
 # Creating the program Gnome shell launcher (.desktop)
-print "### Creating the program Gnome Shell launcher (.desktop)"
+print("### Creating the program Gnome Shell launcher (.desktop)")
 desktoptxt  = "[Desktop Entry]\n"
 desktoptxt += "Type=Application\n"
 desktoptxt += "Version="+VERSION+"\n"
 desktoptxt += "Name=apod-dybg-py\n"
 desktoptxt += "Comment=Astronomy Picture of the Day Dynamic Background.\n"
 desktoptxt += "Path="+HOME+"/.apod-dybg-py\n"
-desktoptxt += "Exec="+HOME+"/.apod-dybg-py/apod-dybg-py.py\n"
+desktoptxt += "Exec="+HprintOME+"/.apod-dybg-py/apod-dybg-py.py\n"
 desktoptxt += "Icon=apod-dybg-py\n"
 desktoptxt += "Terminal=false"
 #desktoptxt += "Name[gl_ES]=apod-dybg"
-print "# Result:"
-print "-"*80
-print desktoptxt
-print "-"*80
+print("# Result:")
+print("-"*80)
+print(desktoptxt)
+print("-"*80)
 
-print "# Copying the apod-dybg-py.desktop to the ~/.local/share/applications folder"
+print("# Copying the apod-dybg-py.desktop to the ~/.local/share/applications folder")
 file=HOME+'/.local/share/applications/apod-dybg-py.desktop'
 f = open(file, 'w')
 f.write(desktoptxt)
 f.close()
 
-print "# Copying the apod-dybg-py.desktop to the ~/.config/autostart folder"
+print("# Copying the apod-dybg-py.desktop to the ~/.config/autostart folder")
 file=HOME+'/.config/autostart/apod-dybg-py.desktop'
 f = open(file, 'w')
 f.write(desktoptxt)
 f.close()
-print ""
+print("")
 
-print "### FINALIZED"
-# print "### END"
+print("### FINALIZED")
+# print("### END")
